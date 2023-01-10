dfc2ac1b757d4bc59c0bb662f22662d840bbd75e
Mirco Gericke <foss@mircogericke.com>
2023-01-10 21:32:47 +0100

    updated assets and manifest

--- Examples/LowLevelPlugin/CustomConnection.cs
+++ Examples/LowLevelPlugin/CustomConnection.cs
@@ -7,7 +7,14 @@ using Microsoft.Extensions.Logging;
 
 using MircoGericke.StreamDeck.Connection;
 using MircoGericke.StreamDeck.Connection.Events;
+using MircoGericke.StreamDeck.Connection.Messages;
 using MircoGericke.StreamDeck.Connection.Model;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 internal class CustomConnection : StreamDeckConnection
 {
@@ -26,19 +33,41 @@ internal class CustomConnection : StreamDeckConnection
 	// each event is an overridable method on the connection
 	protected override async Task OnConnected(EventArgs e, CancellationToken cancellationToken)
 	{
-		// there are methods for each message
-		await LogMessageAsync("Log this!");
-		logger.LogInformation("Connected");
+		// each message has it's own type to use with SendAsync
+		await SendAsync(new LogMessage("Log This!"), cancellationToken);
 	}
 
-	protected override Task OnDeviceDidConnect(DeviceDidConnectEvent e, CancellationToken cancellationToken)
+	protected async override Task OnKeyDown(KeyDownEvent e, CancellationToken cancellationToken)
 	{
-		logger.LogInformation("Connected to device: {deviceName}", e.DeviceInfo.Name);
-		return Task.CompletedTask;
+		// SixLabors.ImageSharp is a fully managed image manipulation library that's easy to use for your stream deck plugin.
+		// Be aware that the library may require a commercial license if used in a commerical environment.
+
+		using var image = new Image<Rgba32>(144, 144, Color.Black);
+		image.Mutate(ctx => ctx
+			.DrawLines(Color.White, 2f, new(0f, 0f), new(144f, 144f))
+			.DrawLines(Color.White, 2f, new(144f, 0f), new(0f, 144f))
+		);
+
+		await SendAsync(new SetImageMessage()
+		{
+			ContextId = e.ContextId,
+			Payload = new()
+			{
+				Image = image.ToBase64String(PngFormat.Instance),
+				Target = SdkTarget.HardwareAndSoftware,
+			}
+		}, cancellationToken);
 	}
 
-	protected async override Task OnWillAppear(WillAppearEvent e, CancellationToken cancellationToken)
+	protected async override Task OnKeyUp(KeyUpEvent e, CancellationToken cancellationToken)
 	{
-		await SetImageAsync("", e.ContextId.ToString(), SdkTarget.HardwareAndSoftware, null);
+		await SendAsync(new SetImageMessage()
+		{
+			ContextId = e.ContextId,
+			Payload = new()
+			{
+				Target = SdkTarget.HardwareAndSoftware,
+			}
+		}, cancellationToken);
 	}
 }
--- Examples/LowLevelPlugin/Program.cs
+++ Examples/LowLevelPlugin/Program.cs
@@ -6,15 +6,15 @@ using Microsoft.Extensions.DependencyInjection;
 
 using MircoGericke.StreamDeck.Hosting;
 
-#if DEBUG
-Debugger.Launch();
-#endif
-
 if (!StreamDeckHost.TryCreateBuilder(args, out var builder))
 {
 	// You could do something else when the application is not started by the stream deck software.
 	return;
 }
+
+#if DEBUG
+Debugger.Launch();
+#endif
 
 builder.Services.AddHostedService<CustomConnection>();
 // add additional services
